93c48eb17a170a0d5e70e7dd8a1f9c276a10959e
Marco-393 <a13marcogil2004@gmail.com>
2026-05-03 09:04:11 -0600

    Update Main.py

--- Simulacion_Scrum-Exam-/Main.py
+++ Simulacion_Scrum-Exam-/Main.py
@@ -1,6 +1,6 @@
-from tarea import Tarea
-from sprint import Sprint
-from tablero import Tablero
+from Estructura_básica import Tarea
+from Estructura_básica import Sprint
+from Funcionalidad_de_completar_tareas import Tablero
 
 # Crear sprint
 sprint1 = Sprint(1)
@@ -11,3 +11,19 @@ t2 = Tarea("Implementar DFS", 1)
 t3 = Tarea("Implementar UCS", 2)
 t4 = Tarea("Pruebas", 3)
 
+# Agregar tareas al sprint
+for t in [t1, t2, t3, t4]:
+    sprint1.agregar_tarea(t)
+
+# Simular avance
+t1.iniciar()
+t1.completar()
+
+t2.iniciar()
+
+# Crear tablero
+tablero = Tablero()
+tablero.actualizar(sprint1.obtener_tareas())
+
+# Mostrar estado
+tablero.mostrar()
