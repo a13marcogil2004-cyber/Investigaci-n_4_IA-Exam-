18aaf26022fcb575a3c11e51edf887963c37ac00
Marco-393 <a13marcogil2004@gmail.com>
2026-05-03 08:53:47 -0600

    Update Main.py

--- Simulacion_Scrum-Exam-/Main.py
+++ Simulacion_Scrum-Exam-/Main.py
@@ -5,3 +5,9 @@ from tablero import Tablero
 # Crear sprint
 sprint1 = Sprint(1)
 
+# Crear tareas de IA
+t1 = Tarea("Implementar BFS", 1)
+t2 = Tarea("Implementar DFS", 1)
+t3 = Tarea("Implementar UCS", 2)
+t4 = Tarea("Pruebas", 3)
+
